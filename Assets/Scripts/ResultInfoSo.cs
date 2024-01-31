e2d3baf7eeb65c9896f162f640d7467acfd5c938
Sergei Shubenkov <sergunwe152@gmail.com>
2024-01-31 22:42:41 +0300

    Added result info

---
+++ Assets/Scripts/ResultInfoSo.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace ToothlessTest
+{
+    [CreateAssetMenu(menuName = "ResultInfoSo")]
+    public class ResultInfoSo : ScriptableObject
+    {
+#if UNITY_EDITOR
+        [field:SerializeField] public Sprite DebugSprite { get; private set; }
+#endif
+        [field:SerializeField] public int Score { get; set; }
+        [field:SerializeField] public string VideoFileName { get; private set; }
+    }
+}
